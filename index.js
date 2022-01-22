14e566287515643a8c8a2e2ca072e2b087540006
João Paulo <jp.cm@hotmail.com>
2022-01-22 10:57:57 -0400

    Master

---
+++ index.js
@@ -0,0 +1,27 @@
+//Jornada do Herói - O Senhor dos Anéis
+/*O Anel de Sauron deve ser destruído pelos corajosos hobbits Frodo e Sam, pois a existência do anel pode causar a ruína de toda a terra média, devido ao seu grande poder. A missão de Frodo e Sam, dois pequenos hobbits do Condado, é destruír o anel na montanha de perdição. Para isso eles contam com a ajuda de uma pequena sociedade, chamada de Sociedade do Anel.
+*/
+
+/*1- Todos os membros participaram da missão?
+2- O anel foi utilizado alguma vez por qualquer membro da sociedade?
+3- Todos os membros da sociedade sobreviveram a missão?
+4- Os inimigos foram todos derrotados?
+5- O anel foi destruído na montanha da perdição?
+*/ 
+htrt
+const prompt = require("prompt-sync")();
+let resp1 = prompt("1- Todos os membros participaram da missão?: ");
+let resp2 = prompt("2- O anel foi utilizado alguma vez por qualquer membro da sociedade?: ");
+let resp3 = prompt("3- Todos os membros da sociedade sobreviveram a missão?: ");
+let resp4 = prompt("4- Os inimigos foram todos derrotados?: ");
+let resp5 = prompt("5- O anel foi destruído na montanha da perdição?: ");
+
+console.log(resp1);
+console.log(resp2);
+console.log(resp3);
+console.log(resp4);
+console.log(resp5);
+
+
+
+
